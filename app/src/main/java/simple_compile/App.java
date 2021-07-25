20818ee2c3294865d78dff017ba4291cec5a2774
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-25 20:54:43 +0530

    updates

---
+++ app/src/main/java/simple_compile/App.java
@@ -0,0 +1,17 @@
+package simple_compile;
+
+import org.apache.arrow.dataset.jni.*;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.dataset.file.*;
+import org.apache.arrow.dataset.scanner.*;
+import org.apache.arrow.dataset.source.*;
+
+
+class App {
+    public static void main(String[] args) {
+        BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+        FileSystemDatasetFactory factory = new FileSystemDatasetFactory(allocator, NativeMemoryPool.getDefault(),
+                FileFormat.PARQUET, "file:///path/to/a/sample.paret");
+    }
+}
