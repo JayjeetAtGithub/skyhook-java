09dd6d6b77124ba93c587d1e4dcb6dac553be45e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-25 22:44:34 +0530

    updates

--- app/src/main/java/simple_compile/App.java
+++ app/src/main/java/simple_compile/App.java
@@ -5,8 +5,33 @@ import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.dataset.file.*;
 import org.apache.arrow.dataset.scanner.*;
-import org.apache.arrow.dataset.source.*;
 
+import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+class ArrowDatasetScanTask implements Runnable {
+    private NativeScanTask scanTask;
+    public ArrowDatasetScanTask(NativeScanTask scanTask) {
+        this.scanTask = scanTask;
+    }
+
+    public NativeScanTask getScanTask() {
+        return this.scanTask;
+    }
+
+    public void run() {
+        try {
+            ScanTask.BatchIterator itr = this.scanTask.execute();
+            while (itr.hasNext()) {
+                System.out.println(itr.next().toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 class App {
     public static void main(String[] args) {
@@ -17,5 +42,16 @@ class App {
         String[] cols = new String[0];
         NativeScanner scanner = dataset.newScan(new ScanOptions(cols, 100000));
         System.out.println(scanner.schema().toString());
+        launchScan(scanner);
+    }
+
+    public static void launchScan(NativeScanner scanner) {
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(16);
+        Iterator<NativeScanTask> itr = (Iterator<NativeScanTask>) scanner.scan().iterator();
+        while(itr.hasNext()){
+            ArrowDatasetScanTask arrowDatasetScanTask = new ArrowDatasetScanTask(itr.next());
+            threadPoolExecutor.execute(arrowDatasetScanTask);
+        }
+        threadPoolExecutor.shutdown();
     }
 }
